300a5b9451e6cd999d4c9a6177346b10949d3d54
yangcs907 <>
2019-02-20 10:50:04 -0500

    initial

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require('path');
+
+
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
+
+// require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
